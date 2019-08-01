4297404576c03a8783a8d22325df81a522033514
Burak Başar <burak.basar@tccb.gov.tr>
2019-08-01 17:51:53 +0300

    update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import Input from './src/Input'
 import PickerList from './src/PickerList'
 import ModalList from './src/ModalList'
 import Switch from './src/Switch'
+import Picker from './src/Picker'
 
 export {
     Input,
     PickerList,
     ModalList,
+    Picker,
     Switch
 }
