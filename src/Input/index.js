15d94a9473026cabc794ecda2aa6520f4c33bcf8
Burak Başar <burak.basar@tccb.gov.tr>
2019-08-01 12:04:24 +0300

    version 0.0.3 input update

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,13 +1,15 @@
 import { Item, Input as InputNativeBae } from 'native-base'
 import React from 'react'
 
-export default function Input({ placeholder, value, keyboardType, onChangeText }) {
+export default function Input({ placeholder, value, keyboardType, onChangeText, clearButtonMode, style }) {
     return (
         <Item style={{ marginLeft: 10 }}>
             <InputNativeBae
                 placeholder={placeholder}
                 keyboardType={keyboardType}
                 value={value}
+                style={style}
+                clearButtonMode={clearButtonMode}
                 onChangeText={onChangeText}
             />
         </Item>
