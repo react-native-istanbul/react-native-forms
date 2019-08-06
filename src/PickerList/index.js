bd9e92ce937ddb24f1fac88cd0750720a0461b3a
Burak Başar <burak.basar@tccb.gov.tr>
2019-08-06 13:24:51 +0300

    List Item separatorTitle eklendi.

--- src/PickerList/index.js
+++ src/PickerList/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, ListItem, Text, Icon, Left, Body, Right } from 'native-base';
+import { Button, ListItem, Text, Icon, Left, Body, Right, Separator } from 'native-base';
 import ModalList from '../ModalList'
 
 export default class PickerList extends Component {
@@ -9,7 +9,7 @@ export default class PickerList extends Component {
     }
 
     render() {
-        const { type, headerTitle, value, iosBarStyle, onChange, data, label } = this.props
+        const { type, headerTitle, value, separatorTitle, iosBarStyle, onChange, data, label } = this.props
         return (
             <React.Fragment>
                 <ModalList
@@ -23,20 +23,44 @@ export default class PickerList extends Component {
                     data={data}
                     close={() => { this.setState({ showModal: false }) }}
                 />
-
+                {
+                    separatorTitle &&
+                    <Separator bordered>
+                        <Text style={{ fontSize: 13 }}>{label}</Text>
+                    </Separator>
+                }
                 <ListItem icon onPress={() => { this.setState({ showModal: true }) }}>
-                    <Left >
-                        <Button style={{ backgroundColor: "#007AFF", display: 'none' }}>
-                            <Icon active name="wifi" />
-                        </Button>
-                    </Left>
-                    <Body style={{ marginLeft: -18 }}>
-                        <Text>{label}</Text>
-                    </Body>
-                    <Right>
-                        <Text>{this.state.seletedItemText}</Text>
-                        <Icon active name="arrow-forward" />
-                    </Right>
+                    {
+                        separatorTitle ?
+                            <React.Fragment>
+                                <Left >
+                                    <Button style={{ backgroundColor: "#007AFF", display: 'none' }}>
+                                        <Icon active name="wifi" />
+                                    </Button>
+                                </Left>
+                                <Body style={{ marginLeft: -18 }}>
+                                    <Text>{this.state.seletedItemText}</Text>
+                                </Body>
+                                <Right>
+                                    <Icon active name="arrow-forward" />
+                                </Right>
+                            </React.Fragment>
+                            :
+                            <React.Fragment>
+                                <Left >
+                                    <Button style={{ backgroundColor: "#007AFF", display: 'none' }}>
+                                        <Icon active name="wifi" />
+                                    </Button>
+                                </Left>
+                                <Body style={{ marginLeft: -18 }}>
+                                    <Text>{label}</Text>
+                                </Body>
+                                <Right>
+                                    <Text>{this.state.seletedItemText}</Text>
+                                    <Icon active name="arrow-forward" />
+                                </Right>
+                            </React.Fragment>
+                    }
                 </ListItem>
             </React.Fragment>
         )
