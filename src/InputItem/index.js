c0d428dff7a02e2974e6cf47634b27f381a07b91
Burak Başar <basarburak@outlook.com>
2019-12-06 10:16:43 +0300

    Release 0.3.5

--- src/InputItem/index.js
+++ src/InputItem/index.js
@@ -3,7 +3,7 @@ import { ListItem, Left, Body, Text, Right, Icon } from 'native-base'
 
 export function InputItem({ children, label }) {
     return (
-        <ListItem style={{ marginLeft : -4, backgroundColor: 'white' }} icon>
+        <ListItem style={{ marginLeft: -4, backgroundColor: 'white' }} icon>
             <Left />
             <Body style={{ backgroundColor: 'white' }}>
                 <Text>{label}</Text>
@@ -17,12 +17,12 @@ export function InputItem({ children, label }) {
 
 export function InputItemText({ onPress, label, seletedItemText }) {
     return (
-        <ListItem style={{ marginLeft : -4, backgroundColor: 'white' }} icon onPress={onPress}>
+        <ListItem style={{ marginLeft: -4, backgroundColor: 'white' }} icon onPress={onPress}>
             <Left />
-            <Body style={{ backgroundColor: 'white'}}>
+            <Body style={{ flex: 0.5, backgroundColor: 'white' }}>
                 <Text>{label}</Text>
             </Body>
-            <Right style={{ backgroundColor: 'white' }}>
+            <Right style={{ flex: 0.5, backgroundColor: 'white' }}>
                 <Text numberOfLines={1}>{seletedItemText}</Text>
                 <Icon active name="arrow-forward" />
             </Right>
@@ -32,7 +32,7 @@ export function InputItemText({ onPress, label, seletedItemText }) {
 
 export function InputItemBody({ onPress, seletedItemText }) {
     return (
-        <ListItem style={{ marginLeft : -4, backgroundColor: 'white' }} icon onPress={onPress}>
+        <ListItem style={{ marginLeft: -4, backgroundColor: 'white' }} icon onPress={onPress}>
             <Left />
             <Body style={{ backgroundColor: 'white' }}>
                 <Text>{seletedItemText}</Text>
--- src/Switch/index.js
+++ src/Switch/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { InputItem } from '../InputItem'
 import { Switch as RNSwitch } from 'react-native'
 
-export default function Switch({ label, value, onValueChange, trackColor }) {
+export default function Switch({ label, value, disabled, onValueChange, trackColor }) {
     return (
         <InputItem
             label={label}
         >
             <RNSwitch
+                disabled={disabled}
                 value={value}
                 trackColor={trackColor}
                 onValueChange={onValueChange}
