d2859a46748304b5a37f25d4ac500024e7e54122
Burak Başar <basarburak@outlook.com>
2020-02-08 22:48:00 +0300

    update example app

--- examples/RNIstanbulFormsExample/App.js
+++ examples/RNIstanbulFormsExample/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { Button } from 'react-native'
+import { Button, ScrollView, SafeAreaView } from 'react-native'
 import { Input, Switch, PickerList, Picker, DateTimePicker } from '@react-native-istanbul/forms'
-import { Container, Header, Title, Content, Separator, Text } from 'native-base'
 
 const darkMode = false
 
@@ -78,87 +77,76 @@ export default class App extends Component {
   render() {
     const { name, saveProfile, city, gender, car, date, nameValidationError } = this.state
     return (
-      <>
-        <Header>
-          <Title>Forms</Title>
-        </Header>
-        <Container>
-          <Content>
-            <Separator>
-              <Text>{''}</Text>
-            </Separator>
-            <PickerList
-              headerTitle={'Select City'}
-              value={city}
-              separatorTitle={true}
-              androidStatusBarColor={'#3949ab'}
-              headerbackgroundColor={'#3949ab'}
-              iosBarStyle={'default'}
-              onChange={(val) => { this.setState({ city: val }) }}
-              data={cityItems}
-              label={'City'}
-            />
-            <Separator>
-              <Text>{''}</Text>
-            </Separator>
-            <PickerList
-              headerTitle={'Select City'}
-              value={car}
-              separatorTitle={false}
-              androidStatusBarColor={'#3949ab'}
-              headerbackgroundColor={'#3949ab'}
-              iosBarStyle={'default'}
-              onChange={(val) => { this.setState({ car: val }) }}
-              data={carsItems}
-              label={'Car'}
-            />
-            <Input
-              placeholder={'Name'}
-              onChangeText={(text) => { this.onChangeName(text) }}
-              value={name}
-              error={nameValidationError}
-            />
-            <Switch
-              label={'Save Profile'}
-              onValueChange={(text) => { this.setState({ saveProfile: text }) }}
-              value={saveProfile}
-            />
+      <SafeAreaView>
+        <ScrollView>
 
-            <Picker
-              label={'Gender'}
-              cancelTitleIOS={'Close'}
-              darkMode={darkMode}
-              doneTitleIOS={'Done'}
-              onValueChange={(val) => {
-                this.setState({ gender: val })
-              }}
-              value={gender}
-              data={genderItems}
-            />
+          <PickerList
+            headerTitle={'Select City'}
+            value={city}
+            separatorTitle={true}
+            androidStatusBarColor={'#3949ab'}
+            headerbackgroundColor={'#3949ab'}
+            iosBarStyle={'default'}
+            onChange={(val) => { this.setState({ city: val }) }}
+            data={cityItems}
+            label={'City'}
+          />
+          <PickerList
+            headerTitle={'Select City'}
+            value={car}
+            separatorTitle={false}
+            androidStatusBarColor={'#3949ab'}
+            headerbackgroundColor={'#3949ab'}
+            iosBarStyle={'default'}
+            onChange={(val) => { this.setState({ car: val }) }}
+            data={carsItems}
+            label={'Car'}
+          />
+          <Input
+            placeholder={'Name'}
+            onChangeText={(text) => { this.onChangeName(text) }}
+            value={name}
+            error={nameValidationError}
+          />
+          <Switch
+            label={'Save Profile'}
+            onValueChange={(text) => { this.setState({ saveProfile: text }) }}
+            value={saveProfile}
+          />
 
-            <DateTimePicker
-              label={'Date'}
-              darkMode={darkMode}
-              cancelTitleIOS={'Close'}
-              doneTitleIOS={'Done'}
-              dateFormat={'DD.MM.YYYY'}
-              androidDisplay={'spinner'}
-              minimumDate={new Date('2000-06-12T14:42:42')}
-              maximumDate={new Date()}
-              onValueChange={(val) => {
-                this.setState({ date: val })
-              }}
-              value={date}
-            />
+          <Picker
+            label={'Gender'}
+            cancelTitleIOS={'Close'}
+            darkMode={darkMode}
+            doneTitleIOS={'Done'}
+            onValueChange={(val) => {
+              this.setState({ gender: val })
+            }}
+            value={gender}
+            data={genderItems}
+          />
 
-            <Button
-              title={'Clear Form'}
-              onPress={() => { this.clearForm() }}
-            />
-          </Content>
-        </Container>
-      </>
+          <DateTimePicker
+            label={'Date'}
+            darkMode={darkMode}
+            cancelTitleIOS={'Close'}
+            doneTitleIOS={'Done'}
+            dateFormat={'DD.MM.YYYY'}
+            androidDisplay={'spinner'}
+            minimumDate={new Date('2000-06-12T14:42:42')}
+            maximumDate={new Date()}
+            onValueChange={(val) => {
+              this.setState({ date: val })
+            }}
+            value={date}
+          />
 
+          <Button
+            title={'Clear Form'}
+            onPress={() => { this.clearForm() }}
+          />
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
