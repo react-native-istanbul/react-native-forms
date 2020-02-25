d6959053f296f110cd29ed1f80edbfcc8477d9d2
Burak Başar <basarburak@outlook.com>
2020-02-25 11:50:44 +0300

    secure tenxt entry

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Item } from 'native-base'
 import { TextInput } from 'react-native'
 
-export default function Input({ placeholder, success, error, value, keyboardType, onChangeText, clearButtonMode, style }) {
+export default function Input({ placeholder, success, error, value, keyboardType, secureTextEntry, onChangeText, clearButtonMode, style }) {
     return (
         <Item
             success={success}
             error={error}
-            style={{ paddingLeft: 10, backgroundColor : 'white' }}>
+            style={{ paddingLeft: 10, backgroundColor: 'white' }}>
             <TextInput
                 placeholderTextColor={'#8F8E95'}
                 value={value}
+                secureTextEntry={secureTextEntry}
                 placeholder={placeholder}
                 style={{ height: 44, flex: 1, marginRight: 10, fontSize: 17 }}
                 clearButtonMode={clearButtonMode}
